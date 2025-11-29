35e62b925624f52f35108120f4073c02d58c21a6
sulejmandev <sulejmandev@outlook.com>
2025-11-29 19:56:09 +0300

    Finshed Project

--- hooks/getSummaryData.ts
+++ hooks/getSummaryData.ts
@@ -1,11 +1,12 @@
 import { connectDB } from '@/lib/DB';
+import { Buyer } from '@/models/Buyer';
 import { Product } from '@/models/Product';
 
 export default async function getSummaryData() {
   await connectDB();
 
   const totalProducts = await Product.countDocuments();
-  const totalBuyers = await Product.countDocuments();
+  const totalBuyers = await Buyer.countDocuments();
 
   return { totalBuyers, totalProducts };
 }
